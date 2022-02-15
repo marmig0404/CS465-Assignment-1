478540f9841d650db78e408452e2e45685389299
Martin Miglio <marmig0404@gmail.com>
2022-02-15 13:33:43 -0500

    added frontend file

---
+++ CS466-W22-IRproject-Group4-FrontEnd.py
@@ -0,0 +1,10 @@
+"""
+# CS465-W22-IRproject-Group4-FrontEnd.py
+#
+# CS465/665, W22, and Project #4
+# Martin Miglio and Johnathan Max Tomlin
+#
+# This file will handle the graphical user interface of this information retrieval program
+"""
+
+pass
