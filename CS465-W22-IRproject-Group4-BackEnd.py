1171651f016ea96afc3d6e27cab81cf806b64896
Martin Miglio <marmig0404@gmail.com>
2022-02-15 13:32:40 -0500

    added backend file

---
+++ CS465-W22-IRproject-Group4-BackEnd.py
@@ -0,0 +1,10 @@
+"""
+# CS465-W22-IRproject-Group4-BackEnd.py
+#
+# CS465/665, W22, and Project #4
+# Martin Miglio and Johnathan Max Tomlin
+#
+# This file will handle the backend processing of this information retrieval program
+"""
+
+pass
